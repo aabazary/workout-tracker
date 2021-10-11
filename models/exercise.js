a5424f017601c8951ad160e3f7954e24e72f8be4
aabazary <aabazary@gmail.com>
2021-10-11 11:27:40 -0700

    created model per seeds data(seeds has exercise as an array)

---
+++ models/exercise.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    exercises: [{
+        name: {
+            type: String,
+            required: "Enter a name for the Workout"
+        },
+        type: {
+            type: Number,
+            required: "Enter a type of Workout"
+        },
+        duration: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+    }],
+    day: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;

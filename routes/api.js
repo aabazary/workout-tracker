78bd3eee87369afa572cbd9e63d711288a77b37c
aabazary <aabazary@gmail.com>
2021-10-11 12:10:45 -0700

    added get routes to workouts, exercise and stats

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,24 @@
 const router = require("express").Router();
 const Workout = require("../models/exercise");
+const path = require("path");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 
+router.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
 
 module.exports = router;
